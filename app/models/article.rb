4917db17d2691fa9613a34dd0a67e6a8a60ae315
David Wilbanks <dwilbank@gmail.com>
2015-10-31 09:39:42 -0700

    update all views with categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
 
   belongs_to :user
+  has_many :article_categories
+  has_many :categories, through: :article_categories
   validates :title, presence: true
   validates :description, presence: true,
                           length: {minimum:10, maximum:300}
